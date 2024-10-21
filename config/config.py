074f3a6e4b83fa9b45547209d07de59ac5a4a8e8
Kristian Peacocke <kpeacocke@ambitiouscake.com>
2024-10-21 22:22:55 +1100

    Fix docker issues
    Used the default network yo

--- config/config.py
+++ config/config.py
@@ -8,7 +8,7 @@ MONGO_PORT = os.getenv("MONGO_PORT", "27017")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "game_database")
 
 # Build the Mongo URI dynamically
-MONGO_URI = f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB_NAME}"
+MONGO_URI = f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB_NAME}?authSource=admin"
 
 # Application secret key for session management
 SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")  # Provide a secure key in the environment
